cc85c1e26dfe3432e4223b2d35a70a56b04b416f
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-10-31 14:29:23 -0400

    Adding occupation variable

--- js/variables.js
+++ js/variables.js
@@ -34,8 +34,7 @@ var age = L.geoJson(age, {
         });
     }
 })
-
-// Adding variables for checkboxes 
+ 
 var education_level = L.geoJson(education_level, {
     pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng, {
@@ -51,6 +50,41 @@ var education_level = L.geoJson(education_level, {
         const popup = `
                 <b>${props.Informant}</b> (<b>${props.County}</b>)
                 <br>Education: ${props.Education}<br> (<b>${props.Sex}</b>)
+            `
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#FFFFFF'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#FFFFFF'
+            });
+        });
+    }
+})
+
+var occupation = L.geoJson(occupation, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#808080',
+            weight: 1,
+            fillColor: '#FFFFFF',
+            fillOpacity: .8,
+            radius: 10
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+                <b>${props.Informant}</b> (<b>${props.County}</b>)
+                <br>Occupation: ${props.Occupation}<br> (<b>${props.Sex}</b>)
             `
         layer.bindTooltip(popup, {
             className: 'tool-informant'
