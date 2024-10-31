1bcdb19f980eca33f7722fb8f0515622adc046c2
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-10-31 13:21:50 -0400

    Adding age variable
    Starting variable js by adding age

---
+++ js/variables.js
@@ -0,0 +1,36 @@
+// Adding variables for checkboxes 
+var age = L.geoJson(age, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#808080',
+            weight: 1,
+            fillColor: '#FFFFFF',
+            fillOpacity: .8,
+            radius: 10
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+                <b>${props.Informant}</b> (<b>${props.County}</b>)
+                <br>Age: ${props.age}<br>
+                <br>Gender: ${props.Sex}</br>
+            `
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#FFFFFF'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#FFFFFF'
+            });
+        });
+    }
+})
