49bba2df162184bcc660e9e456148a02f8c68b48
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-10-31 14:06:31 -0400

    Adding education variable

--- js/variables.js
+++ js/variables.js
@@ -34,3 +34,40 @@ var age = L.geoJson(age, {
         });
     }
 })
+
+// Adding variables for checkboxes 
+var education_level = L.geoJson(education_level, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#808080',
+            weight: 1,
+            fillColor: '#FFFFFF',
+            fillOpacity: .8,
+            radius: 10
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+                <b>${props.Informant}</b> (<b>${props.County}</b>)
+                <br>Age: ${props.Education}<br>
+                <br>Gender: ${props.Sex}</br>
+            `
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#FFFFFF'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#FFFFFF'
+            });
+        });
+    }
+})
