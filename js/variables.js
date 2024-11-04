a1b0d1b7b578bd11ff6943980776abb96f5fde40
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2024-11-04 13:34:34 -0500

    Add informant variable

--- js/variables.js
+++ js/variables.js
@@ -1,4 +1,39 @@
 // Adding variables for checkboxes 
+var informant = L.geoJson(informant, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#808080',
+            weight: 1,
+            fillColor: '#000066',
+            fillOpacity: .8,
+            radius: 10
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+                <b>${props.Informant}</b>
+                <br>County: ${props.County}<br>
+            `
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#000066'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#000066'
+            });
+        });
+    }
+})
+
 var age = L.geoJson(age, {
     pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng, {
